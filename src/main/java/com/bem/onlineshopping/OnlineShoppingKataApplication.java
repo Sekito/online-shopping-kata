82b54382aacd0b6aa7e746f050010a4b0f1b9501
Sekito <bentouri.elmehdi@gmail.com>
2024-10-05 11:30:23 +0100

    intial commit

---
+++ src/main/java/com/bem/onlineshopping/OnlineShoppingKataApplication.java
@@ -0,0 +1,13 @@
+package com.bem.onlineshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bem/onlineshopping/OnlineShoppingKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bem.onlineshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
